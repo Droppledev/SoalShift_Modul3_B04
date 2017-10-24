71e7c75fc31d37d78ab4f10585d7a073d36a8398
itsmerafi <32842792+itsmerafi@users.noreply.github.com>
2017-10-25 01:11:39 +0700

    Update soal4.c
    Belom dicoba. Ada kemungkinan tidak urut

--- soal4.c
+++ soal4.c
@@ -4,20 +4,35 @@
 #include <unistd.h>
 
 pthread_t tid[1000];
- 
-void faktorial () {
-    
+int n, greater;
+status = 0
+void faktorial (*ar) {
+    int j;
+    long long restult =1;
+        for (j=tid[i]; j>=2; j--){
+            result *=j; 
+         }
+         printf ("Hasil %d!= %lld", i, result);
+        }
+    }
+    status =i;
+    //printf("\n");
+}
     
 }
 
 
 int main (int argc, char argv[]{
     int i;
-    
+    n = argc;
+    greater =0; 
     for (i=1; i<=argc ; i++){
-        pthread_create (&(tid[i]), NULL, &faktorial, NULL ) 
+        pthread_create (&(tid[i]), NULL, &faktorial, NULL );
+        if (greater < argv[i]){
+            greater = argv[i];//terbesar
+        }
     }
-    
+     
     for (i=1; i<=argc ; i++){
         pthread_join(tid[i],NULL);
     }
