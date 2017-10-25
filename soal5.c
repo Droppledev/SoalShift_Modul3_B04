ef33f7a62e158be8ab002581f08aea544df124e2
Ayas Faikar Nafis <ayasfn@gmail.com>
2017-10-25 11:06:01 +0700

    Update soal5.c
    belum menggunakan thread, masih kodingan c biasa

--- soal5.c
+++ soal5.c
@@ -9,10 +9,26 @@ char str[1500]; // untuk nyimpen seluruh kata yang ada di dalam file
 char arr[250][30]; // untuk dipecah-pecah
 
 int main (int argc, char argv[]){
+    int i,j,k;
+    j=k=0;
     FILE* novel = fopen("Novel.txt","r");
     while (fgets(str,1500,novel)!=NULL){
         
+    for (i=0;str[i]!='\0';i++){
+            if (str[i]==' ' || str[i]==',' || str[i]=='.'){
+                j++;
+                k=0;
+            }
+            else {
+                arr[j][k] = str[i];
+                k++;
+            }
+        }
+        j++; // jumlah kata = jumlah spasi + 1;
     }
-    int i;
-
+    fclose(novel);
+    for (i=1;i<argc;i++){
+        printf("%s = %d\n",argv[i],find(argv,i,j));
+    }
+    return 0;
 } 
