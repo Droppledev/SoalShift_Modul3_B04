2bc0b84a33ec9d164cebf876b5116e4d72ce9120
itsmerafi <32842792+itsmerafi@users.noreply.github.com>
2017-10-25 12:14:16 +0700

    Update soal5.c

--- soal5.c
+++ soal5.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-#include <pthread.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+pthread_t tid[100];
 
-pthread_t tid[2];
-char str[1500]; // untuk nyimpen seluruh kata yang ada di dalam file
-char arr[250][30]; // untuk dipecah-pecah
-
-int find(char *argv[],int indeks, int jumkata){
-    int i,counter=0;
-    for (i=0;i<jumkata;i++){
-        if (strcmp(arr[i],argv[indeks])==0){
-            counter++;
-        }
+void *read_words (void *arg ) {
+    FILE *novel = fopen ("Novel.txt", "r");
+    char x[128];
+    char *string = (char *) arg;
+    int count =0;
+    
+    while (fscanf(novel, "%s", x) == 1) {
+        if (strstr (string, x))
+            count++;
     }
-    return counter;
+    printf ("%d\n", count);
+    
 }
 
 int main (int argc, char *argv[]){
-    int i,j,k;
-    j=k=0;
-    FILE* novel = fopen("Novel.txt","r");
-    while (fgets(str,1500,novel)!=NULL){  
-        for (i=0;str[i]!='\0';i++){
-            if (str[i]==' ' || str[i]==',' || str[i]=='.'){
-                j++;
-                k=0;
-            }
-            else {
-                arr[j][k] = str[i];
-                k++;
-            }
-        }
-        j++; // jumlah kata = jumlah spasi + 1;
+    int i;
+    char *str;
+    for (i=1; i<argc ; i++){
+        str =  argv[i];
+        pthread_create (&(tid[i]), NULL, &read_words, (void *) str);
     }
-    fclose(novel);
-    for (i=1;i<argc;i++){
-        printf("%s = %d\n",argv[i],find(argv,i,j));
+     
+    for (i=1; i<argc ; i++){
+        pthread_join(tid[i],NULL);
     }
-    return 0;
-} 
+    
+}
+// Kekurangan ada pada "Fina,", koma ikut terbaca.
