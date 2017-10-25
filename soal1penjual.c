715ffb1bfcd989bc4da1f3b6db4b6be59479ba8c
itsmerafi <32842792+itsmerafi@users.noreply.github.com>
2017-10-25 17:38:26 +0700

    Update soal1penjual.c

--- soal1penjual.c
+++ soal1penjual.c
@@ -6,10 +6,15 @@
 
 
 char weapon[6][10] ={"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3","MINE"};
-int *quantity ;
+int quantity[6] = {NULL,0,0,0,0,0,0}    ;
 
 void ListSenjata () {
-    
+    int i;
+    for (i=0; i<6; i++){
+        if (quantity[i+1]>0) 
+            printf("%s: %d\n", weapon[i], quantity[i+1]);
+        
+    }
 }
 
 void tambahStock (){
@@ -18,25 +23,36 @@ void tambahStock (){
     int i;
     printf ("Masukkan nama senjata dan jumlah: ");
     scanf ("%s %d", &name, &amount);
-    
-    for (i=0; i<5; i++){
-       if( strcmp (weapon[i], name));
-            quantity[i] += amount;
+     
+    for (i=0; i<6; i++){
+       // printf ("%s %s ", weapon[i], name);
+       if( strcmp (weapon[i],name)==0){
+            quantity[i+1] += amount;
+            printf ("Jumlah senjata saat ini %s: %d\n", weapon[i], quantity[i+1]);
+       }    
     }
     
 }
 int main (){
     int menu;
+    int i;
     key_t key = 1234;
     int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
-    quantity = shmat(shmid, NULL, 0);
+    *quantity = shmat(shmid, NULL, 0);
     //printf ("%d ", quantity[3]);
     //printf ("%d ", quantity[6]);
-    shmdt(quantity);
+    shmdt(*quantity);
     shmctl(shmid, IPC_RMID, NULL);
+    
+    
+    
     while (1){
+        for (i=0; i<6; i++)
+            printf("%s: %d\n", weapon[i], quantity[i+1]);
+        
+       
         printf ("1. Stock senjata\n2. tambah stock senjata\n");
-        printf ("Pilih Menu 1 atau 2: ");
+        printf ("Pilih Menu 1 atau 2: ") ;
         scanf ("%d", &menu);
         switch (menu){
             case 1:
@@ -48,9 +64,3 @@ int main (){
         }
     }
 }
-
-
-
-
-
-
