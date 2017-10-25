66bef5e0987fd7a91c94d2b3d667b9019aead04e
itsmerafi <32842792+itsmerafi@users.noreply.github.com>
2017-10-25 17:04:04 +0700

    Update and rename soal1penjual to soal1penjual.c

--- soal1penjual.c
+++ soal1penjual.c
@@ -5,32 +5,38 @@
 #include <string.h>
 
 
-char weapon[6][10] ={"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3","MINE"}
-int quantity[6] 
+char weapon[6][10] ={"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3","MINE"};
+int *quantity ;
 
 void ListSenjata () {
     
 }
 
-int tambahStock (){
-    char name[];
+void tambahStock (){
+    char name[10];
     int amount;
     int i;
-    scanf ("%s %d", &nama, &amount);
+    printf ("Masukkan nama senjata dan jumlah: ");
+    scanf ("%s %d", &name, &amount);
     
     for (i=0; i<5; i++){
        if( strcmp (weapon[i], name));
             quantity[i] += amount;
     }
     
-    
-    
 }
 int main (){
     int menu;
+    key_t key = 1234;
+    int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
+    quantity = shmat(shmid, NULL, 0);
+    //printf ("%d ", quantity[3]);
+    //printf ("%d ", quantity[6]);
+    shmdt(quantity);
+    shmctl(shmid, IPC_RMID, NULL);
     while (1){
+        printf ("1. Stock senjata\n2. tambah stock senjata\n");
         printf ("Pilih Menu 1 atau 2: ");
-        printf ("1. Stock senjata\n2. tambah stock senjata");
         scanf ("%d", &menu);
         switch (menu){
             case 1:
@@ -40,8 +46,6 @@ int main (){
                 tambahStock();
                 break;
         }
-        
-        
     }
 }
 
