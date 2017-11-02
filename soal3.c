ebf72ef8fec3b4a2377ddac3eeb8a6e47bbceb61
Ayas Faikar Nafis <ayasfn@gmail.com>
2017-11-02 22:58:10 +0700

    Update soal3.c
    agar scanf tidak menunggu, tetapi jika salah satu thread exit, semua exit

--- soal3.c
+++ soal3.c
@@ -8,33 +8,35 @@
 pthread_t tid[3];
 int statusL = 100;
 int statusK = 100;
-
+//int pil;
 
 void *lohan (void *arg) {
     
     while(1){
-        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100){
-            break;
-        }
+        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100 == 1) {
+                printf("\nGame Berakhir !\n");
+                break;
+            }
         else{
             sleep(10);
             statusL-=15;
         }
     }
-    
+    exit(EXIT_SUCCESS);
 }
 
 void *kepiting (void *arg) {
     while(1){
-        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100){
-            break;
-        }
+        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100 == 1) {
+                printf("\nGame Berakhir !\n");
+                break;
+            }
         else{
             sleep(20);
             statusK-=10;
         }
     }
-    
+    exit(EXIT_SUCCESS);
 }
 
 void cekStatus(){
@@ -43,16 +45,16 @@ void cekStatus(){
     printf("Status Kepiting = %d\n\n",statusK);
 }
 
-int main(){
-    system("clear");
-    pthread_create (&(tid[0]), NULL, &lohan, NULL);
-    pthread_create (&(tid[1]), NULL, &kepiting, NULL);
-    
+void *mainMenu(void *arg){
+    //int *pil=arg;
+    int pil;
     while(1){
-        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100) break;
+        if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100 == 1) {
+            printf("\nGame Berakhir !\n");
+            break;
+        }
         else{
-           int pil;
-           char c;
+          // char c;
             
             printf("1. Beri Makan Lohan\n");
             printf("2. Beri Makan Kepiting\n");
@@ -60,23 +62,27 @@ int main(){
             printf("-------------------------\n");
             printf("Pilih Nomor (1-3) > ");
             scanf("%d",&pil);
-            if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100) break;
+             if (statusL <= 0 || statusL >100 || statusK <= 0 || statusK >100 == 1) {
+                printf("\nGame Berakhir !\n");
+                break;
+            }
             else {
                 if (pil==1){
                     printf("\nLohan diberi makan !\n");
                     printf("Status lohan bertambah 10 !");
                     statusL+=10;
-                    cekStatus();
+                    //cekStatus();
                     
                 }
                 else if (pil==2){
                     printf("\nKepiting diberi makan !\n");
                     printf("Status kepiting bertambah 10 !");
                     statusK+=10;
-                    cekStatus();
+                    //cekStatus();
                 }
                 else if (pil==3){
                     cekStatus();
+                    
                 }
                 else{
                     printf("\nPilihan yang dimasukkan salah\n");
@@ -87,5 +93,19 @@ int main(){
         }
         
     }
-    printf("Game Berakhir !\n");
+    exit(EXIT_SUCCESS);
+}
+
+int main(){
+    system("clear");
+    pthread_create (&(tid[0]), NULL, &lohan, NULL);
+    pthread_create (&(tid[1]), NULL, &kepiting, NULL);
+    pthread_create (&(tid[2]), NULL, &mainMenu, NULL);
+
+    
+    pthread_join(*tid, NULL);
+    //pthread_join(tid[1], NULL);
+    //pthread_join(tid[2], NULL);
+    
+    printf("\nGame Berakhir !\n");
 }
